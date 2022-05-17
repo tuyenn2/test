0508d1c29928bdeea5bc73797e5a4798e914a3f2
unknown <tuyen231102b@gmail.com>
2022-05-17 21:42:22 +0700

    first commit

---
+++ csriptt.js
@@ -0,0 +1,72 @@
+// document.getElementById("doanVan").textContent = "new paragra";
+// document.getElementById("doanVan").innerHTML = "<b> new padsad </b>";
+// let doanVan =document.getElementById("doanVan");
+
+// doanVan.textContent="new";
+
+
+// let paragrahs=document.getElementsByTagName('p');
+// paragrahs[1].textContent = 'new';
+// paragrahs[1].style.backgroundColor ='yellow';
+
+
+// querySlector // bo chon dau tien;
+// querySlectorAll // bo chon tat ca;
+
+
+// //buoc 1 xac dinh cac phan tu tuong tac
+
+// let doanVan=document.querySelector('p');
+// let nutnhan =document.querySelector('button');
+
+// // buoc 2 bat su kien
+// nutnhan.addEventListener('click',function()
+// {
+//     doanVan.style.backgroundColor='yellow';                                                                     
+// })
+
+
+//b1 xac dinh cac phan tu tac dong
+// let nutnhan=document.querySelector('p');
+// let canhchua=document.querySelector('.parent');
+
+// //b2 xu ly su kien
+// nutnhan.addEventListener('click',function()
+// {
+//     let canhmoi =document.createElement('p');
+//     canhmoi.textContent='child...';
+//     canhmoi.className="child";
+// })
+
+// let txtfurt=document.getElementsByClassName('txt-fruit');
+// let btn =document.getElementsByClassName('btnShowfruit');
+// let img=document.getElementsByClassName('pictures');
+// let array=new array['banana','orange','lemon','strawberry'];
+
+
+// if(textfurt.value == '')
+// {
+//     alert('ban chua nhap gia tri');
+// }
+// else
+// {
+//     let araay =['banana','orange','lemon','strawberry']
+
+//     let cheack=false;
+//     if(let i=0;i<array.length;i++)
+//     {
+//         if(array[i]==textfurt.value)
+//         {
+//             cheack=true;
+//             break;
+//         }
+//     }
+// }
+
+//e.preventdefault cham cua form  
+// alert(slitour.selected)
+
+
+
+ 
+
